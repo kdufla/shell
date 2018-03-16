a63dd80a237356d9da4385142cec10e005c08848
Babdus <babdusi@gmail.com>
2018-03-16 23:18:14 +0000

    Make ulimit sets work also

--- ulimit.c
+++ ulimit.c
@@ -4,7 +4,7 @@ typedef struct ulimit_resource {
   char symbol;
   int  resource;
   char *name;
-  int  devisor;
+  int  divisor;
 } ulimit_resource_t;
 
 ulimit_resource_t resource_dict[] = {
@@ -29,8 +29,9 @@ ulimit_resource_t resource_dict[] = {
 typedef struct ulimit_value {
   int           used;
   char          *name;
-  int           devisor;
+  int           divisor;
   struct rlimit *rlim;
+  int           resource;
 } ulimit_value_t;
 
 int resource_lookup(char symbol) {
@@ -59,12 +60,16 @@ int cmd_ulimit(struct tokens *tokens){
   int value_i = 0;
   // int curr_resource = NULL;
   int hardness = 0;
-  int a_is_met = 0;
-  int rv = -1;
+  int a_is_met = -1;
+  int is_set   = -1;
+  int rv = 0;
   if (args_len == 1) {
     fprintf(stdout, "unlimited\n");
   }
   for (int i = 1; i < args_len; i++) {
+    if (rv == -1) {
+      break;
+    }
     char *arg = tokens_get_token(tokens, i);
     if (arg[0] == '-') {
       if (strlen(arg) == 2) {
@@ -80,28 +85,30 @@ int cmd_ulimit(struct tokens *tokens){
             ulimit_resource_t resource = resource_dict[j];
             values[value_i].rlim = malloc(sizeof(struct rlimit));
             values[value_i].used = 0;
+            values[value_i].resource = resource.resource;
             rv = getrlimit(resource.resource, values[value_i].rlim);
             if(rv == 0){
               values[value_i].name = resource.name;
-              values[value_i].devisor = resource.devisor;
+              values[value_i].divisor = resource.divisor;
               value_i++;
             }
             else {
               fprintf(stderr, "%s\n", strerror(errno));
             }
           }
-          a_is_met = 1;
+          a_is_met = 0;
         }
-        else if (a_is_met == 0) {
+        else if (a_is_met == -1) {
           int resource_i = resource_lookup(arg[1]);
           if (resource_i != -1) {
             ulimit_resource_t resource = resource_dict[resource_i];
             values[value_i].rlim = malloc(sizeof(struct rlimit));
+            values[value_i].used = 0;
+            values[value_i].resource = resource.resource;
             rv = getrlimit(resource.resource, values[value_i].rlim);
             if(rv == 0){
-              values[value_i].used = 0;
               values[value_i].name = resource.name;
-              values[value_i].devisor = resource.devisor;
+              values[value_i].divisor = resource.divisor;
               value_i++;
             }
             else {
@@ -119,11 +126,32 @@ int cmd_ulimit(struct tokens *tokens){
         rv = -1;
       }
     }
-    else if ( strcmp(arg, "unlimited") == 0 ) {
-
-    }
-    else if ( is_number(arg) == 0 ) {
-
+    else if ( a_is_met == -1 && (strcmp(arg, "unlimited") == 0 || is_number(arg) == 0) ) {
+      if (value_i > 0 && values[value_i-1].used == 0) {
+        uintmax_t num;
+        if (strcmp(arg, "unlimited") == 0) {
+          num = (uintmax_t)RLIM_INFINITY;
+        }
+        else{
+          num = strtoumax(arg, NULL, 10) * values[value_i-1].divisor;
+        }
+        struct rlimit *rlim = malloc(sizeof(struct rlimit));
+        if (hardness == 0){
+          rlim->rlim_cur = (rlim_t)num;
+          rlim->rlim_max = values[value_i-1].rlim->rlim_max;
+        }
+        else{
+          rlim->rlim_cur = values[value_i-1].rlim->rlim_cur;
+          rlim->rlim_max = (rlim_t)num;
+        }
+        const struct rlimit *rlim1 = rlim;
+        rv = setrlimit(values[value_i-1].resource, rlim1);
+        if (rv == -1) {
+          fprintf(stderr, "%s\n", strerror(errno));
+        }
+        is_set = 0;
+        free(rlim);
+      }
     }
     else {
       fprintf(stderr, "Invalid option\n");
@@ -133,7 +161,7 @@ int cmd_ulimit(struct tokens *tokens){
 
   for (int i = 0; i < 14; i++) {
     if (values[i].used == 0) {
-      if (rv == 0) {
+      if (rv == 0 && is_set != 0) {
         struct rlimit *rlim = values[i].rlim;
         rlim_t lim;
         if (hardness == 0) {
@@ -146,7 +174,7 @@ int cmd_ulimit(struct tokens *tokens){
           fprintf(stdout, "%s unlimited\n", values[i].name);
         }
         else {
-          fprintf(stdout, "%s %ju\n", values[i].name, (uintmax_t)lim/values[i].devisor);
+          fprintf(stdout, "%s %ju\n", values[i].name, (uintmax_t)lim/values[i].divisor);
         }
       }
       free(values[i].rlim);
