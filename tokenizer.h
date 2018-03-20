89fcab43e883ed3061c55c514b0bb13c8ac524f8
aleksandreK <alkakh14@freeuni.edu.ge>
2018-03-20 21:27:57 +0000

    Add tokenizer 2

--- tokenizer.h
+++ tokenizer.h
@@ -4,7 +4,7 @@
 struct tokens;
 
 /* Turn a string into a list of words. */
-struct tokens *tokenize(const char *line);
+struct tokens *tokenize(const char *line, const char* delim);
 
 /* Tokenize using given delimiter string. */
 struct tokens *tokenize_str(const char *line, const char *delim);
